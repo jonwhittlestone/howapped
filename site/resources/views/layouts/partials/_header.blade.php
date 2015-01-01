50a161c28005be5a42de02fd87973729f1fb0c7d
Jon Whittlestone <dev@howapped.com>
2015-01-01 17:02:57 +0000

    slogan media query

--- site/resources/views/layouts/partials/_header.blade.php
+++ site/resources/views/layouts/partials/_header.blade.php
@@ -10,7 +10,7 @@
                             <span class="icon-bar"></span>
                             </button>
                             <a class="navbar-brand" href="/">How<span>Apped.</span></a>
-                            <div class='slogan' style='margin: 8px 0 0 1em;padding: 12px 0 8px 1em;border-left: 1px solid #e6e6e6;float: left; margin-top:29px'>Full-stack Web Development in London, UK with Laravel and Rails</div>
+                            <div class='slogan'>Full-stack Web Development in London, UK with Laravel and Rails</div>
                         </div>
                         <?php //include 'partials/_nav.php'; ?>
                         <!-- /.navbar-collapse -->
